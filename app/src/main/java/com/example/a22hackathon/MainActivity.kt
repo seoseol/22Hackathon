88daf2093a26fb51327d51d1c0fe0c02bd65ea90
seoseol <qwejkl0203@naver.com>
2022-07-06 21:16:00 +0900

    add bottom navigation

--- app/src/main/java/com/example/a22hackathon/MainActivity.kt
+++ app/src/main/java/com/example/a22hackathon/MainActivity.kt
@@ -2,14 +2,52 @@ package com.example.a22hackathon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.buv_main) as BottomNavigationView
+        bottomNav.setOnNavigationItemReselectedListener {
+            when(it.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.nav_book -> {
+                    loadFragment(BookFragment())
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.nav_book_report -> {
+                    loadFragment(BookReportFragment())
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.nav_calendar -> {
+                    loadFragment(CalendarFragment())
+                    return@setOnNavigationItemReselectedListener
+                }
+
+            }
+        }
 
 
     }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+
 }
